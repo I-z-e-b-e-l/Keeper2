e14a7628d7ebcd8e126c88acb9927ca8a7b20e36
izebel <izebel@rocketmail.com>
2022-11-25 13:55:42 -0500

    map through notes to generate Note components

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function Note(){
+    return(
+        <div className="note" >
+            <h1>This is a Title</h1>
+            <p>This is a paragraph</p>
+        </div>
+    )
+}
+
+export default Note;
